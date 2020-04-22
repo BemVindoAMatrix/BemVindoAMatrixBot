fa1b48420a6d4938ff13e35b2af72d018600b70c
newmankr <fabricionewman@gmail.com>
2020-04-22 11:31:01 +0000

    Add unban command

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -24,4 +24,31 @@ const banUser = async (ctx) => {
     }
 }
 
-module.exports = banUser
+const unbanUser = async (ctx) => {
+    const chatId = ctx.message.chat.id
+    const userId = ctx.message.reply_to_message 
+                ? ctx.message.reply_to_message.from.id 
+                : ctx.message.from.id
+    const userFirstName = ctx.message.reply_to_message 
+                ? ctx.message.reply_to_message.from.first_name 
+                : ctx.message.from.first_name
+
+    if(await isAdmin(ctx)){
+        try{
+            await ctx.telegram.unbanChatMember(chatId, userId)
+        
+            ctx.reply(`${userFirstName} unbanned.`)
+        } catch(e) {
+            console.log(e)
+    
+            ctx.reply('Sorry, I couldn\'t unban this user.')
+        }
+    } else {
+        ctx.reply('You can\'t do that, you\'re not admin.')
+    }
+}
+
+module.exports = {
+    banUser,
+    unbanUser
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 
 const Telegraf = require('telegraf')
 
-const { kickUser, banUser } = require('./commands/index')
+const { kickUser, banUser, unbanUser } = require('./commands/index')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.command('kick', (ctx) => kickUser(ctx))
    .command('ban', (ctx) => banUser(ctx))
+   .command('unban', (ctx) => unbanUser(ctx))
 
 bot.startPolling()
 
